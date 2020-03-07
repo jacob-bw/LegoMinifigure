270f3ea5ca88409e012bf11aa8505e32948c9b07
Jacob <wittenberg.jacob@gmail.com>
2020-03-07 10:08:19 -0600

    Add project files.

---
+++ LegoMinifigure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegoMinifigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
