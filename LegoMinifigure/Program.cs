2361128fd9282b2711e8c78d25bec9b5bd002529
Jacob <wittenberg.jacob@gmail.com>
2020-03-07 12:02:13 -0600

    pushing inclass work

--- LegoMinifigure/Program.cs
+++ LegoMinifigure/Program.cs
@@ -6,7 +6,28 @@ namespace LegoMinifigure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var astronaut = new SpaceGuy("Space Person", "Space Janitor");
+            var astronaut2 = new SpaceGuy("Space Lady", "Space Commander");
+
+            astronaut.DoYourJob();
+            astronaut2.DoYourJob();
+
+            // can't do this
+            // astronaut.Job = "Commander";
+
+            // also can't do this
+            // astronaut.SuitedUp = false;
+
+            var casper = new Ghost("Casper", DateTime.Today)
+            // this is an Object Initalizer
+            {
+                Friendly = true,
+            };
+
+            casper.Spook();
+            casper.Haunt("Whipstaff Manor");
+            casper.Spook();
+
         }
     }
 }
